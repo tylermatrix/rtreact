219dd6edd07d494b366917ddc8a9f93cf9affa19
Tyler Maatrix <tylermaatrix@gmail.com>
2021-03-27 13:47:44 -0400

    added ways to input data into component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 
 const title = "react";
 
+const welcome = {
+  greeting: "hey",
+  title: "react",
+};
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello {title}</h1>
+      <h1>Hello {getTitle("react")}</h1>
 
       <label htmlFor="serach">Search: </label>
       <input type="text" id="search" />
