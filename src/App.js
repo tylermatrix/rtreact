25f83716832180e441f7c6e9a096f63897cab455
tyler matrix <tylermaatrix@gmail.com>
2021-05-19 12:52:58 -0400

    fetching data from an api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const initialStories = [
   {
@@ -74,11 +75,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
