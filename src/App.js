96e4b422c7794ede2fa20012b8961e3fcff1be3e
tyler matrix <tylermaatrix@gmail.com>
2021-05-19 12:58:22 -0400

    memoizing the fetch function and side effects

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ const App = () => {
     isLoading: false,
     isError: false,
   });
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
@@ -87,6 +87,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
